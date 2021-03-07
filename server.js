e1febf00b869b69d39ee5b61881683679321a24a
root <root@krekerr85.fvds.ru>
2021-03-07 13:52:43 -0500

    nice

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port =80
 var morgan = require('morgan')
 app.use(morgan('combined'))
 app.get('/', (req, res) => {
