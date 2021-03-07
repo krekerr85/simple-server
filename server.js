b317a11fb90eafa307211e8dee14761f9ed153db
artandsky <krekerr85@gmail.com>
2021-03-07 21:43:32 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 80
+var morgan = require('morgan')
+app.use(morgan('combined'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log(req.ip)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
